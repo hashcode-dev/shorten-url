2ec7be3343fc67c631b1e32abb97da42a0eda935
Sahu, Gaurav <hashcode.dev@gmail.com>
2025-06-10 21:26:46 +0530

    First Commit

--- src/main/java/com/hashcode/shortenurl/ShortenUrlApplication.java
+++ src/main/java/com/hashcode/shortenurl/ShortenUrlApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShortenUrlApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ShortenUrlApplication.class, args);
     }
-
 }
