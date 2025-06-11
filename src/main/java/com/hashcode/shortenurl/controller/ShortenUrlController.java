6f2fdf8293f7a27f37cb11553ebbda0d7701a569
Sahu, Gaurav <hashcode.dev@gmail.com>
2025-06-11 10:44:14 +0530

    URL Shortener Project

--- src/main/java/com/hashcode/shortenurl/controller/ShortenUrlController.java
+++ src/main/java/com/hashcode/shortenurl/controller/ShortenUrlController.java
@@ -1,9 +1,33 @@
 package com.hashcode.shortenurl.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.hashcode.shortenurl.model.ShortUrl;
+import com.hashcode.shortenurl.service.ShortenUrlService;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("shortenUrl")
+@Getter
 public class ShortenUrlController {
+
+    @Autowired
+    ShortenUrlService shortenUrlService;
+
+
+    @PostMapping("/shorten")
+    public ResponseEntity<ShortUrl> shortenUrl(@RequestBody ShortUrl originalUrl) {
+        ShortUrl shortUrl = getShortenUrlService().shortenUrl(originalUrl);
+        return new ResponseEntity<>(shortUrl, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{shortUrl}")
+    public ResponseEntity<String> redirect(@PathVariable String shortUrl) {
+        ShortUrl url = getShortenUrlService().findByShortUrl(shortUrl);
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .header("Location", url.getOriginalUrl())
+                .build();
+    }
 }
