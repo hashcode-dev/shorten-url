2cbe84f300e7303ed0d90d0acb81e6aa7af07f5b
Ayushvi Sahu <ayushvisahu58@gmail.com>
2026-05-16 01:15:44 +0530

    NA

--- src/main/java/com/hashcode/shortenurl/repository/ShortenUrlMongoRepository.java
+++ src/main/java/com/hashcode/shortenurl/repository/ShortenUrlMongoRepository.java
@@ -1,7 +1,16 @@
 package com.hashcode.shortenurl.repository;
 
 import com.hashcode.shortenurl.model.ShortenUrl;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ShortenUrlMongoRepository extends MongoRepository<ShortenUrl, String> {
+
+    long countByStatus(String status);
+
+    @Aggregation(pipeline = {
+            "{ $group: { _id: null, total: { $sum: \"$clicks\" } } }"
+            "{ $project: { _id: 0, total: 1 } }"
+    })
+    long getTotalClicks();
 }
